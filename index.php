36cbf4f900ab50d18c7e8fae74273076bd8d511e
Musa Imad Issa <musaissa004@gmail.com>
2023-10-01 00:06:32 +0100

    implement tcpdf in the repo

--- index.php
+++ index.php
@@ -16,11 +16,19 @@ $conn = mysqli_connect("localhost", "musaissa15", "sql123", "phase2");
     $result_check = mysqli_num_rows($results);
     if ($result_check > 0) {
         while($row = mysqli_fetch_assoc($results)){
-            
-echo $row['food_id'] . " " . $row['food_name'] . " " . $row['food_type'];
+    
+
+         echo $row['food_id'] . " " . $row['food_name'] . " " . $row['food_type'] .  "<br>";
+       
+
 
         }
     }
     ?>
+
+    <form method="POST" action="pdf.php" target="_blank"> 
+        <input type="submit" name="pdf_creater" value="PDF">
+    </form>
+
 </body>
 </html>
